a197a35ff25d22e97f833925c3f675b5bc9c37b3
Ville <Ville@VILLE-PC>
2017-02-19 19:29:59 +0200

    Teht01FileApp done

---
+++ Teht01FileApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Teht01FileApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string data = " ";
+            while (true) { 
+            
+            using (StreamWriter w = File.AppendText("data.txt"))
+            {
+
+                if (data == "end") { 
+                    w.Close();
+                    break;
+                    }
+                    else { 
+                    Console.WriteLine("Type the lines here, type end to stop writing");
+                    data = Console.ReadLine();
+                    w.WriteLine(data);
+                    }
+
+                }
+            }
+
+
+            
+            try
+            {
+                string text = System.IO.File.ReadAllText("data.txt");
+                System.Console.WriteLine("Contents of data.txt:\n" + text);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found (FileNotFoundException)");
+            }
+
+
+        }
+        }
+
+    }
+
